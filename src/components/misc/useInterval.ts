535ee217cb35eb8b8ff1322f538feb45b94a8f57
tris <nexus4fortune@gmail.com>
2024-05-19 12:53:01 +0900

    コメントを追加

--- src/components/misc/useInterval.ts
+++ src/components/misc/useInterval.ts
@@ -1,5 +1,11 @@
 import { useEffect, useRef } from "react";
 
+/**
+ * 一定間隔で実行する間隔を登録するカスタムフック。
+ *
+ * @param callback 一定間隔で呼び出す関数
+ * @param fps (およそ)この間隔で実行する frame per seconds の値
+ */
 export function useInterval(callback: () => void, fps = 10) {
   const ref = useRef(callback);
 
